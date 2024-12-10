97fd5326c26d3267472d1ef86d69b26addcd4114
dernewt <dernewt@gmail.com>
2024-12-10 08:28:09 -0500

    clean up warning that itself has a buggy warning

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -25,12 +25,18 @@ public class Day3
     const string OpMul = "mul";
     public record Command(string Name, int? Arg1 = null, int? Arg2 = null);
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance",
+        "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.",
+        Justification = "Readability over performance, may be more dynamic in the future.")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality",
+        "IDE0079:Remove unnecessary suppression.",
+        Justification = "IDE BUG SYSLIB1045 is seen as IDE0079 even though it's not.")]
     public static IEnumerable<Command> ParseCommands(string input)
     {
         var matches = Regex.Matches(input,
-            @"(?<op>" + OpMul + @")\((?<arg1>\d{1,3}),(?<arg2>\d{1,3})\)" +
-            @"|(?<op>" + OpDo + @")\(\)" +
-            @"|(?<op>" + OpDont + @")\(\)");
+            "(?<op>" + OpMul + @")\((?<arg1>\d{1,3}),(?<arg2>\d{1,3})\)" +
+            "|(?<op>" + OpDo + @")\(\)" +
+            "|(?<op>" + OpDont + @")\(\)");
 
         var commands = matches.Select(m => new Command(
             m.Groups["op"].Value,
