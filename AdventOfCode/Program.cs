2315b0d463b9c7bbbf33a471e86cb2945e626f46
dernewt <dernewt@gmail.com>
2024-12-09 21:50:56 -0500

    fun little reflection to run all Day.Run looking methods

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,15 @@
 ﻿using AdventOfCode;
+using static System.Reflection.BindingFlags;
 
-Day2.Run();
+var days = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(t => t.GetTypes())
+    .Where(t => t.Namespace == nameof(AdventOfCode) && t.Name.StartsWith("Day"))
+    .OrderBy(t => t.Name)
+    .Select(t => t.GetMethod(nameof(Day1.Run), Public|Static)!);
+
+foreach (var day in days)
+{
+    Console.WriteLine($"{day.DeclaringType!.Name}: {day.Invoke(null, null)}");
+}
+
+
