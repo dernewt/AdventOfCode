b858d76b9782492c35b2a4098a6c8acb3664f7dc
dernewt <dernewt@gmail.com>
2024-12-10 08:16:14 -0500

    day 3

---
+++ AdventOfCode.Tests/Day3Tests.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode.Tests;
+
+public class Day3Tests
+{
+    [Fact]
+    public void IntegrationTest()
+    {
+        var result = Day3.Run();
+        result.MultiplicationsSum.Should().Be(174960292);
+
+        result.ConditionalMultiplicationsSum.Should().Be(56275602);
+    }
+
+    [Fact]
+    public void ParseMulTest()
+    {
+        var input = @"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        var commands = Day3.ParseCommands(input);
+        commands.Should().BeEquivalentTo<Day3.Command>([new("mul", 2, 4), new("mul", 5, 5), new("mul", 11, 8), new("mul", 8, 5)]);
+    }
+
+    [Fact]
+    public void ParseMulDoDontTest()
+    {
+        var input = @"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        var commands = Day3.ParseCommands(input);
+        commands.Should().BeEquivalentTo<Day3.Command>([new("mul", 2, 4), new("don't"), new("mul", 5, 5), new("mul", 11, 8), new("do"), new("mul", 8, 5)]);
+    }
+
+    [Fact]
+    public void ExecuteMultipicationsTest()
+    {
+        var results = Day3.ExecuteMultipications([
+            new("mul", 0, 0),
+            new("poo", 3, 3), //skip
+            new("mul", 2, 2),
+        ]);
+        results.Should().BeEquivalentTo([
+            0,
+            4,
+        ]);
+    }
+
+    [Fact]
+    public void ExecuteEnabledMultipicationsTest()
+    {
+        var results = Day3.ExecuteEnabledMultipications([
+            new("mul", 2, 4), //yes
+            new("don't", 0, 0), //disable
+            new("mul", 5, 5), //no
+            new("mul", 11, 8), //no
+            new("do", 0, 0), //enable
+            new("mul", 8, 5) //yes
+        ]);
+        results.Should().BeEquivalentTo([
+            8,
+            40,
+        ]);
+    }
+
+}
---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day3
+{
+    public record Results(int MultiplicationsSum, int ConditionalMultiplicationsSum);
+
+    public static Results Run()
+    {
+        var commands = ParseCommands(File.ReadAllText("Day3.cs.txt"));
+
+        //part 1
+        var multiplicationResults = ExecuteMultipications(commands);
+
+        //part 2
+        var multiplicationConditionalResults = ExecuteEnabledMultipications(commands);
+
+        return new(multiplicationResults.Sum(), multiplicationConditionalResults.Sum());
+    }
+
+    const string OpDo = "do";
+    const string OpDont = "don't";
+    const string OpMul = "mul";
+    public record Command(string Name, int? Arg1 = null, int? Arg2 = null);
+
+    public static IEnumerable<Command> ParseCommands(string input)
+    {
+        var matches = Regex.Matches(input,
+            @"(?<op>" + OpMul + @")\((?<arg1>\d{1,3}),(?<arg2>\d{1,3})\)" +
+            @"|(?<op>" + OpDo + @")\(\)" +
+            @"|(?<op>" + OpDont + @")\(\)");
+
+        var commands = matches.Select(m => new Command(
+            m.Groups["op"].Value,
+            m.Groups["arg1"].Success ? int.Parse(m.Groups["arg1"].Value) : null,
+            m.Groups["arg2"].Success ? int.Parse(m.Groups["arg2"].Value) : null
+            ));
+
+        return commands;
+    }
+
+    public static IEnumerable<int> ExecuteMultipications(IEnumerable<Command> commands)
+    {
+        return commands
+            .Where(p => p.Name == OpMul)
+            .Select(p => p.Arg1!.Value * p.Arg2!.Value);
+    }
+
+    public static IEnumerable<int> ExecuteEnabledMultipications(IEnumerable<Command> commands)
+    {
+        var isEnabled = true;
+        var filtered = commands.Where(cmd =>
+        {
+            isEnabled = cmd.Name switch
+            {
+                OpDont => false,
+                OpDo => true,
+                _ => isEnabled
+            };
+            return isEnabled;
+        });
+
+        return ExecuteMultipications(filtered);
+    }
+}
