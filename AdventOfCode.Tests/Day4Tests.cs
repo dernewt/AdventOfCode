4b275b63645cd1e7d5da1f760e3a1687e9372e5d
dernewt <dernewt@gmail.com>
2024-12-14 22:14:05 -0500

    day4, punted part 2 there is a better solution with a similar approach without subgrids but it didn't jump out at me fast enough.

---
+++ AdventOfCode.Tests/Day4Tests.cs
@@ -0,0 +1,153 @@
+﻿namespace AdventOfCode.Tests;
+
+public class Day4Tests
+{
+    [Fact]
+    public void IntegrationTest()
+    {
+        var result = Day4.Run();
+        result.XmasCount.Should().Be(2521);
+        result.MasXCount.Should().Be(1912);
+    }
+
+    [Fact]
+    public void ColumnsSquareTest()
+    {
+        var input = new LetterGrid([
+            "012",
+            "345",
+            "678",
+        ]);
+        input.Columns().Should().BeEquivalentTo([
+            "036",
+            "147",
+            "258"]);
+    }
+
+    [Fact]
+    public void ColumnsRectangleTest()
+    {
+        var input = new LetterGrid([
+            "01",
+            "34",
+            "67",
+        ]);
+
+        input.Columns().Should().BeEquivalentTo([
+            "036",
+            "147"]);
+    }
+
+    [Fact]
+    public void ForwardDiagnalTest()
+    {
+        var input = new LetterGrid([
+            "012", 
+            "345",
+            "678",
+        ]);
+
+        input.ForwardDiagnals().Should().BeEquivalentTo([
+            "0",
+            "31",
+            "642",
+            "75",
+            "8"]);
+    }
+
+    [Fact]
+    public void ForwardDiagnalRectangleTest()
+    {
+        var input = new LetterGrid([
+            "01",
+            "34",
+            "67",
+        ]);
+
+        input.ForwardDiagnals().Should().BeEquivalentTo([
+            "0",
+            "31",
+            "64",
+            "7"]);
+    }
+
+    [Fact]
+    public void BackDiagnalTest()
+    {
+        var input = new LetterGrid([
+            "012",
+            "345",
+            "678",
+        ]);
+
+        input.BackDiagnals().Should().BeEquivalentTo([
+            "2",
+            "51",
+            "840",
+            "73",
+            "6"]);
+    }
+
+    [Fact]
+    public void CountSmallXMasTest()
+    {
+        var input = new LetterGrid([
+            "M.S",
+            ".A.",
+            "M.S"]);
+        Day4.CountMasX(input).Should()
+            .Be(1);
+    }
+
+    [Fact]
+    public void CountXMasTest()
+    {
+        var input = new LetterGrid([
+            ".M.S......",
+            "..A..MSMS.",
+            ".M.S.MAA..",
+            "..A.ASMSM.",
+            ".M.S.M....",
+            "..........",
+            "S.S.S.S.S.",
+            ".A.A.A.A..",
+            "M.M.M.M.M.",
+            ".........."]);
+        Day4.CountMasX(input).Should()
+            .Be(9);
+    }
+
+    [Fact]
+    public void CountSmallXmasTest()
+    {
+        var input = new LetterGrid([
+            "..X...",
+            ".SAMX.",
+            ".A..A.",
+            "XMAS.S",
+            ".X...."]);
+
+        Day4.CountXmas(input).Should()
+            .Be(4);
+    }
+
+    [Fact]
+    public void CountXmasTest()
+    {
+        var input = new LetterGrid([
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX"]);
+
+        Day4.CountXmas(input).Should()
+            .Be(18);
+    }
+
+}
---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,143 @@
+﻿
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public static partial class Day4
+{
+    public record Results(int XmasCount, int MasXCount);
+    public static Results Run()
+    {
+        var grid = new LetterGrid(File.ReadAllLines("Day4.cs.txt"));
+
+        return new(CountXmas(grid), CountMasX(grid));
+    }
+
+
+    public static int CountXmas(LetterGrid grid)
+    {
+        var horizontalCount = grid.Rows()
+            .Sum(text => ReversableOverlappingXmas().Matches(text).Count);
+
+        var verticalCount = grid.Columns()
+            .Sum(text => ReversableOverlappingXmas().Matches(text).Count);
+
+        var diagCount = grid.ForwardDiagnals()
+            .Sum(text => ReversableOverlappingXmas().Matches(text).Count);
+
+        var backDiagCount = grid.BackDiagnals()
+            .Sum(text => ReversableOverlappingXmas().Matches(text).Count);
+
+        return horizontalCount + verticalCount + diagCount + backDiagCount;
+    }
+
+    public static int CountMasX(LetterGrid grid)
+    {
+        var count = 0;
+
+        foreach (var subgrid in grid.SubGrids33())
+        {
+            // GOTCHA below is overkill for the subgrid approach
+            // there is a more elogant way on the whole grid at once
+            // but I just did this special case of 3x3 grids
+            var forwardDiagMatches = subgrid.ForwardDiagnals()
+                .Select(text => ReversableOverlappingMas().Matches(text)
+                    .Select(m => m.Index).ToArray());
+
+            var backDiagMatches = subgrid.BackDiagnals()
+                .Select(text => ReversableOverlappingMas().Matches(text)
+                    .Select(m => m.Index).ToArray());
+
+            count += forwardDiagMatches.SelectMany(m=>m)
+                .Intersect(backDiagMatches.SelectMany(m=>m))
+                .Count();
+        }
+        return count;
+    }
+
+    [GeneratedRegex("(?<=X)MAS|(?<=S)AMX")]
+    private static partial Regex ReversableOverlappingXmas();
+
+    [GeneratedRegex("(?<=M)AS|(?<=S)AM")]
+    private static partial Regex ReversableOverlappingMas();
+}
+
+public class LetterGrid(string[] grid)
+{
+    public IEnumerable<string> Rows() => grid;
+    public IEnumerable<string> Columns()
+    {
+        for (var rowIndex = 0; rowIndex < grid.First().Length; rowIndex++)
+        {
+            var vertical = new char[grid.Length];
+            for (var columnIndex = 0; columnIndex < grid.Length; columnIndex++)
+            {
+                vertical[columnIndex] = grid[columnIndex][rowIndex];
+            }
+            yield return new(vertical);
+        }
+    }
+
+    public IEnumerable<string> BackDiagnals()
+    {
+        for (var (row, col) = (0, grid[0].Length - 1); row < grid.Length && col >= 0;)
+        {
+            yield return BackDiagnalFromBottom(row, col);
+
+            if (row == grid.Length - 1)
+                col--;
+            else
+                row++;
+        }
+    }
+
+    public IEnumerable<string> ForwardDiagnals()
+    {
+        for (var (row, col) = (0, 0); row < grid.Length && col < grid[row].Length;)
+        {
+            yield return ForwardDiagnalFromBottom(row, col);
+
+            if (row == grid.Length - 1)
+                col++;
+            else
+                row++;
+        }
+    }
+
+    public string BackDiagnalFromBottom(int startRow, int startCol)
+    {
+        var result = new StringBuilder();
+        for (var (row, col) = (startRow, startCol); row >= 0 && col >= 0;)
+        {
+            result.Append(grid[row--][col--]);
+        }
+        return result.ToString();
+    }
+    public string ForwardDiagnalFromBottom(int startRow, int startCol)
+    {
+        var result = new StringBuilder();
+        for (var (row, col) = (startRow, startCol); row >= 0 && col < grid[row].Length;)
+        {
+            result.Append(grid[row--][col++]);
+        }
+        return result.ToString();
+    }
+
+    public IEnumerable<LetterGrid> SubGrids33()
+    {
+        var width = 3;
+        var height = 3;
+
+        for (var row = 0; row < grid.Length && row + height <= grid.Length; row++)
+        {
+            for (var col = 0; col < grid[row].Length && col + width <= grid[row].Length; col++)
+            {
+                yield return new(
+                    grid.Skip(row).Take(height)
+                    .Select(r => r[col..(col+width)])
+                    .ToArray());
+            }
+        }
+    }
+}
